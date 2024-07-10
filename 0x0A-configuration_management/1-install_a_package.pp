d097ff97e9c9f4e51b88b2f363c6c5a4b8eb8e08
Lex9jar <alexanderedim89@gmail.com>
2024-07-10 19:10:03 +0100

    Installs 'flask' using Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install 'flask' using Puppet
+
+package { 'python3.8':
+  ensure   => 'present'
+}
+
+package { 'python3-pip':
+  ensure   => 'presnet'
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['flask']
 }
