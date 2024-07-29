58ef405c5239e5537568cba9565f018302d738e2
LexUpgrade <alexanderedim89@gmail.com>
2024-07-30 00:17:18 +0100

    Records all taks of all employees from a REST API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,14 +12,14 @@ for usr in users:
     todos = get("https://jsonplaceholder.typicode.com/todos/?userId={}".format(
                 usr.get("id"))).json()
     usr_todos = list()
-    for l in todos:
+    for i in todos:
         d = dict()
         d['username'] = usr.get('username')
-        for k, v in l.items():
+        for k, v in i.items():
             if k in ['title', 'completed']:
                 d[k] = v
         usr_todos.append(d)
-    full_list.update({str(usr.get("id")): usr_todos})
+    full_list.update({usr.get("id"): usr_todos})
 
 with open("todo_all_employees.json", mode="w", newline="") as file:
     json.dump(full_list, file)
