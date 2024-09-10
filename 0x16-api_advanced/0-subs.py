925a3dbc04f4ff38ecdf793017500463fdc4075e
LexUpgrade <alexanderedim89@gmail.com>
2024-09-10 12:18:42 +0100

    Queries a Reddit API and return the number of subscribers of a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function <number_of_subscribers>."""
 
+
 def number_of_subscribers(subreddit):
     """Queries a Reddit API and return the number of subscribers of a
     given subreddit.
