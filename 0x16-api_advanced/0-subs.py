c62fa7532ef428be3c336d6a70ec29666ec5e905
LexUpgrade <alexanderedim89@gmail.com>
2024-09-10 12:22:24 +0100

    Queries a Reddit API and return the number of subscribers of a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Defines a function <number_of_subscribers>."""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries a Reddit API and return the number of subscribers of a
     given subreddit.
     """
-    import requests
-
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': "linux:0x16-api_advanced:v1.0.0 (by u/lex9jar)"}
 
